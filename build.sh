254bbd62867f815e5530996a62cbbe5ea69e3d00
Xì Gà <vandug23@gmail.com>
2024-12-26 15:30:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,30 +2,43 @@
 
 pacman -Sy --needed --noconfirm git glibc docker docker-compose
 git clone https://aur.archlinux.org/mongodb.git
+chown -R nobody:nobody mongodb
 
 cat > Dockerfile << EOF
 FROM archlinux:base
 
 RUN pacman-key --init && \
     pacman -Sy --noconfirm archlinux-keyring && \
-    pacman -Sy --needed --noconfirm boost-libs curl libstemmer pcre snappy yaml-cpp boost python-cheetah3 python-psutil python-pymongo python-regex python-requests python-setuptools python-yaml git glibc patch binutils fakeroot sudo clang
+    pacman -Sy --needed --noconfirm boost-libs curl libstemmer pcre snappy yaml-cpp boost \
+        python-cheetah3 python-psutil python-pymongo python-regex python-requests python-setuptools python-yaml \
+        git glibc patch binutils fakeroot sudo clang mongo-c-driver gperftools libunwind \
+        pkgconf
 
-CMD cd `pwd`/mongodb && \
-    chown -R nobody:nobody /mongodb && \
-    su nobody -s /bin/bash -c makepkg
+USER nobody
+WORKDIR /mongodb
+RUN makepkg
 EOF
+
 docker build -t mongodb-builder .
 nproc=$(nproc)
 result=$(awk "BEGIN {printf \"%.1f\", 0.95 * $nproc}")
-docker run -v `pwd`/mongodb:`pwd`/mongodb --cpus=$result mongodb-builder
+docker run -v `pwd`/mongodb:/mongodb --cpus=$result mongodb-builder
 if [[ $? -eq 0 ]]; then
-    cd mongodb
-    source PKGBUILD
-    srcdir=`pwd`/src
-    package && \
-    docker rmi mongodb-builder && \
-    rm ./Dockerfile
-    pacman -Sy --needed --noconfirm boost-libs curl libstemmer pcre snappy yaml-cpp glibc
+    echo "***************"
+    echo "*   SUCCESS   *"
+    echo "***************"
+    echo ""
+    echo "To install do:"
+    echo "
+move mongodb /
+cd /mongodb
+source PKGBUILD
+srcdir=\`pwd\`/src
+package && \\
+docker rmi mongodb-builder && \\
+rm ./Dockerfile
+pacman -Sy --needed --noconfirm boost-libs curl libstemmer pcre snappy yaml-cpp glibc
+"
 else
     echo Build failed
 fi
